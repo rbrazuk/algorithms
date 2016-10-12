bba1b66ebddf28394151774f6bea8e28420f47f6
Ross Brazuk <ross.brazuk@gmail.com>
2016-10-12 10:31:30 -0400

    initial commit

---
+++ euler_swift/euler.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+ 
+ Multiples of 3 and 5
+ Problem 1
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ 
+ Find the sum of all the multiples of 3 or 5 below 1000.
+ 
+ */
+
+func sums3sAnd5s() -> Int {
+    var total = 0
+    
+    for number in 1..<1000 where number % 3 == 0 || number % 5 == 0 {
+        total += number
+        }
+    return total
+}
+
+sums3sAnd5s()
